9d66ec7487ea3790338abc03c57b48ce67c5a290
rymbil <rymbil@mail.ru>
2018-11-05 22:06:33 +0500

    Сделал

---
+++ CommandMaker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommandMaker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            if(args.Length == 1)
+            {
+                try
+                {
+                    switch (args[0])
+                    {
+                        case "VolumeUp": QueueManager.VolumeUp(); break;
+                        case "VolumeDown": QueueManager.VolumeDown(); break;
+                        case "VolumeOff": QueueManager.VolumeOff(); break;
+                        case "Shutdown": QueueManager.Shutdown(); break;
+                        case "MicOn": QueueManager.MicOn(); break;
+                        case "MicOff": QueueManager.MicOff(); break;
+                        case "CancelShutdown": QueueManager.CancelShutdown(); break;
+                        default: MessageBox.Show($"Unknown command {args[0]}"); break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+
+            }
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
---
+++ CommandMaker/QueueManager.cs
@@ -0,0 +1,74 @@
+﻿using CommandMessageNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandMaker
+{
+    static class QueueManager
+    {
+        private const string queueName = @".\Private$\OlegQueue";        
+
+        private static MessageQueue GetMessageQueue()
+        {
+            MessageQueue messageQueue = new MessageQueue(queueName);
+            messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(CommandMessage) });
+            return messageQueue;
+        }
+
+        public static void VolumeUp()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            messageQueue.Send(new CommandMessage() { command = CommandMessage.Command.VolumeUp });
+            messageQueue.Close();
+        }
+
+        public static void VolumeDown()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            messageQueue.Send(new CommandMessage() { command = CommandMessage.Command.VolumeDown });
+            messageQueue.Close();
+        }
+
+        public static void CancelShutdown()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            messageQueue.Send(new CommandMessage() { command = CommandMessage.Command.CancelShutdown });
+            messageQueue.Close();
+        }
+
+        public static void MicOff()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            messageQueue.Send(new CommandMessage() { command = CommandMessage.Command.MicOff });
+            messageQueue.Close();
+        }
+
+
+        public static void MicOn()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            messageQueue.Send(new CommandMessage() { command = CommandMessage.Command.MicOn });
+            messageQueue.Close();
+        }
+
+        public static void Shutdown()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            messageQueue.Send(new CommandMessage() { command = CommandMessage.Command.Shutdown });
+            messageQueue.Close();
+        }
+
+        public static void VolumeOff()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            messageQueue.Send(new CommandMessage() { command = CommandMessage.Command.VolumeOff });
+            messageQueue.Close();
+        }
+
+
+    }
+}
